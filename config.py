ec3741d976780407386271f1516b085f5a93fa4c
yotamtb <yotam.tayeb@gmail.com>
2018-06-03 17:33:05 +0300

    add aws credentials

--- config.py
+++ config.py
@@ -4,3 +4,8 @@ EMAIL_SERVER = 'smtp.office365.com'
 
 EMAIL_QUERY_DATE_FORMAT = "%d-%b-%Y"
 EMAIL_DOWNLOAD_FILE_NAME = 'email_id_{}.eml'
+
+
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+BUCKET_NAME = ''
